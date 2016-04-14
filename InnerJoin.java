0e690758158f1fec0de980a5417988ca0b8ccc24
Shivani Tyagi <tashivani@123gmail.com>
2016-04-14 17:36:34 -0500

    first commit

---
+++ InnerJoin.java
@@ -0,0 +1,17 @@
+// • Idea:
+// for each tuple r in R do
+// 	for each tuple s in S do
+// 		if r, s satisfies join cond, then
+// 		add r, s to result
+
+
+// • For each tuple in the outer relation R, we scan the
+// entire inner relations S
+
+public class InnerJoin
+{
+	public static void main(String[] args)
+	{
+		
+	}
+}
